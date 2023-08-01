72634a27e5d3fed7b37001599ebe4729b602fc8b
gaurav sharma <shar.work.gaurav@gmail.com>
2023-08-01 23:03:35 +0530

    Delete MicroServices/Admin directory

--- MicroServices/Admin/src/main/java/com/casestudy/Admin/DAO/AdminDAO.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy.Admin.DAO;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.casestudy.Admin.Models.Admin;
-
-public interface AdminDAO extends MongoRepository<Admin, String> {
-
-}
